68b505970c34576b975a9571b3352eef349d2770
carterchri21 <carterchri21@students.ecu.edu>
2021-09-16 21:25:12 -0400

    assignment #1

---
+++ ConditionalPractice.m
@@ -0,0 +1,11 @@
+inputNumber = 11;
+
+if (inputNumber > 10)
+    inputNumber = inputNumber + 5;
+elseif (inputNumber < 10)
+    inputNumber = inputNumber - 5; 
+else 
+    inputNumber;
+end
+
+inputNumber
